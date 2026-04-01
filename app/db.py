5740684c9304ed2c169ddd4c4c8e7dd155a1ebde
Henry <50325487+hkrogell@users.noreply.github.com>
2026-04-01 22:42:05 +0300

    added database and reworked /rooms route to use db instead of sample data

---
+++ app/db.py
@@ -0,0 +1,35 @@
+import os
+import psycopg
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+def get_conn():
+    return psycopg.connect(DATABASE_URL, autocommit=True, row_factory=psycopg.rows.dict_row)
+
+def create_schema():
+    with get_conn() as conn, conn.cursor() as cur:
+        # Create the schema
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS hotel_rooms (
+                id SERIAL PRIMARY KEY,
+                room_number INTEGER NOT NULL,
+                type VARCHAR(50) NOT NULL,
+                price NUMERIC(10, 2) NOT NULL
+            );
+            
+            CREATE TABLE IF NOT EXISTS hotel_guests (
+                id SERIAL PRIMARY KEY,
+                firstname VARCHAR(100) NOT NULL,
+                lastname VARCHAR(100) NOT NULL,
+                address VARCHAR(255)
+            );
+                    
+            CREATE TABLE IF NOT EXISTS hotel_bookings (
+                id SERIAL PRIMARY KEY,
+                guest_id INTEGER NOT NULL REFERENCES hotel_guests(id),
+                room_id INTEGER NOT NULL REFERENCES hotel_rooms(id),
+                datefrom DATE NOT NULL,
+                dateto DATE NOT NULL,
+                addinfo VARCHAR(255)
+            );
+        """)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
+from app.db import get_conn, create_schema
 
 app = FastAPI()
 
@@ -14,16 +15,25 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-rooms = [
-    { "room_number": 1, "room_type": "Suite", "room_price": 1000},
-    { "room_number": 2, "room_type": "Double Bed", "room_price": 500},
-    { "room_number": 3, "room_type": "Presidential", "room_price": 10000},
-]
+# create database schema
+create_schema()
 
 @app.get("/")
 def read_root():
-    return { "msg": "Welcome to the hotel booking-API" }
+    # test database
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("""
+            SELECT 'database working' as msg, version() AS version
+        """)
+        db_status = cur.fetchone();
+    return { "msg": "Welcome to the hotel booking-API", "db": db_status}
 
 @app.get("/rooms")
 def get_rooms():
-    return rooms
+    with get_conn() as conn, conn.cursor() as cur:
+        cur.execute("""
+            SELECT room_number, type AS room_type, price AS room_price
+            FROM hotel_rooms 
+            ORDER BY room_number
+        """)
+        return cur.fetchall()
