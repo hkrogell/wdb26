e4f0a77f628d46169688a0ac7f928875588aec0c
Henry <50325487+hkrogell@users.noreply.github.com>
2026-04-01 03:33:58 +0300

    Added /ip route with HTMLResponse showing client IP

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -9,3 +10,11 @@ def read_root():
 @app.get("/hello")
 def hello():
     return { "msg": "heyheyhey" }
+
+@app.get("/api/ip")
+def get_ip(request: Request):
+    return { "ip": request.client.host}
+
+@app.get("/ip", response_class=HTMLResponse)
+def ip(request: Request):
+    return f"<h1>Din ip är {request.client.host}</h1>"
