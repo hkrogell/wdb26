e6dc74ba25b0925a9764230c95a1b471d53ce57a
Henry <50325487+hkrogell@users.noreply.github.com>
2026-04-01 22:52:52 +0300

    Revert "added database and reworked /rooms route to use db instead of sample data"
    This reverts commit 5740684c9304ed2c169ddd4c4c8e7dd155a1ebde.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
-from app.db import get_conn, create_schema
 
 app = FastAPI()
 
@@ -15,25 +14,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# create database schema
-create_schema()
+rooms = [
+    { "room_number": 1, "room_type": "Suite", "room_price": 1000},
+    { "room_number": 2, "room_type": "Double Bed", "room_price": 500},
+    { "room_number": 3, "room_type": "Presidential", "room_price": 10000},
+]
 
 @app.get("/")
 def read_root():
-    # test database
-    with get_conn() as conn, conn.cursor() as cur:
-        cur.execute("""
-            SELECT 'database working' as msg, version() AS version
-        """)
-        db_status = cur.fetchone();
-    return { "msg": "Welcome to the hotel booking-API", "db": db_status}
+    return { "msg": "Welcome to the hotel booking-API" }
 
 @app.get("/rooms")
 def get_rooms():
-    with get_conn() as conn, conn.cursor() as cur:
-        cur.execute("""
-            SELECT room_number, type AS room_type, price AS room_price
-            FROM hotel_rooms 
-            ORDER BY room_number
-        """)
-        return cur.fetchall()
+    return rooms
